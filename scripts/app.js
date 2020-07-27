cbb615d422c38f76274fd0187a6c15422beecc0e
bocheng24 <jeffcgp@hotmail.com>
2020-07-27 23:08:27 +0800

    Add getQuote method

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-window.console.log("window is calling");
+async function getQuote() {
+    const apiURL = 'https://api.forismatic.com/api/1.0//?method=getQuote&lang=en&format=json';
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    catch(e) {
+        console.log(e);
+        getQuote();
+    }
+}
+
+getQuote();
