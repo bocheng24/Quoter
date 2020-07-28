1cddd5b290189b29aa781d358fb31211905144e0
bocheng24 <jeffcgp@hotmail.com>
2020-07-28 16:33:49 +0800

    Fix API CAlling issue

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,7 @@ async function getQuote() {
     try {
         const response = await fetch(proxyURL + apiURL);
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         return data;
     }
 
@@ -21,17 +21,15 @@ async function getQuote() {
     }
 }
 
-getQuote();
-
-const data = '{"quoteText":"A lot of people give up just before theyre about to make it. You know you never know when that next obstacle is going to be the last one.  ", "quoteAuthor":"Chuck Norris", "senderName":"", "senderLink":"", "quoteLink":"http://forismatic.com/en/2bc6cf0fd5/"}'
+// const data = '{"quoteText":"A lot of people give up just before theyre about to make it. You know you never know when that next obstacle is going to be the last one.  ", "quoteAuthor":"Chuck Norris", "senderName":"", "senderLink":"", "quoteLink":"http://forismatic.com/en/2bc6cf0fd5/"}'
 
 newQuoteBtn.addEventListener('click', () => {
-    data = getQuote();
+    const data = getQuote();
     console.log(data);
-    const dataJson = JSON.parse(data);
-    quoteText.innerText = dataJson.quoteText;
+    // const dataJson = JSON.parse(data);
+    quoteText.innerText = data.quoteText;
 
-    quoteAuthor = dataJson.quoteAuthor === "" ? "Unknow" : dataJson.quoteAuthor;
+    quoteAuthor = data.quoteAuthor === "" ? "Unknow" : data.quoteAuthor;
 
     author.innerText = `- ${quoteAuthor}`;
 })
