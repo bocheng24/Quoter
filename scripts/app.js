b487a8ecc87766ea932cca94da82e2fcbb01065a
bocheng24 <jeffcgp@hotmail.com>
2020-07-28 17:54:05 +0800

    Add Loader

--- scripts/app.js
+++ scripts/app.js
@@ -3,20 +3,34 @@ const quoteText = document.getElementById('quote-text');
 const author = document.getElementById('author');
 const newQuoteBtn = document.getElementById('quote-new');
 const tweetBtn = document.getElementById('twitter-btn');
+const loader = document.querySelector('.loader');
+
+const turnLoaderOn = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+const turnLoaderOff = () => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 
 async function getQuote() {
     const apiURL = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     const proxyURL = 'https://cors-anywhere.herokuapp.com/'
     
     try {
+        turnLoaderOn();
+
         const response = await fetch(proxyURL + apiURL);
         const data = await response.json();
-        // console.log(data);
+        
         quoteText.innerText = data.quoteText;
-
         quoteAuthor = data.quoteAuthor === "" ? "Unknow" : data.quoteAuthor;
-    
         author.innerText = `- ${quoteAuthor}`;
+        
+        turnLoaderOn();
     }
 
     catch(e) {
@@ -25,9 +39,8 @@ async function getQuote() {
     }
 }
 
-// const data = '{"quoteText":"A lot of people give up just before theyre about to make it. You know you never know when that next obstacle is going to be the last one.  ", "quoteAuthor":"Chuck Norris", "senderName":"", "senderLink":"", "quoteLink":"http://forismatic.com/en/2bc6cf0fd5/"}'
-
-newQuoteBtn.addEventListener('click', getQuote)
+window.addEventListener('load', turnLoaderOff);
+newQuoteBtn.addEventListener('click', getQuote);
 
 tweetBtn.addEventListener('click', () => {
     const quoteText = document.getElementById('quote-text').innerText;
@@ -35,4 +48,4 @@ tweetBtn.addEventListener('click', () => {
 
     const tweetURL = `https://twitter.com/intent/tweet?text=${quoteText} ${quoteAuthor}`;
     window.open(tweetURL, '_blank');
-})
+});
